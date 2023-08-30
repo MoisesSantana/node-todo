ed302f128b8441d9af4d2312494bc687fe196f0a
Moisés Santana <moisaant@gmail.com>
2023-08-30 20:59:24 -0300

    feat(service): create

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,34 @@
+import { completeTask, createNewTask, deleteTask, getAllTasks, updateTask } from './service/tasks.js';
 import { buildRoutePath } from './utils/build-route-path.js';
 
 export const routes = [
-  { method: 'GET', path: buildRoutePath('/tasks'), handler: (req, res) => {
-    return res.end('path: ' + req.url + ' method: ' + req.method);
-  } },
-  { method: 'POST', path: buildRoutePath('/tasks'), handler: (req, res) => {
-    return res.end('path: ' + req.url + ' method: ' + req.method);
-  } },
-  { method: 'PUT', path: buildRoutePath('/tasks/:id'), handler: (req, res) => {
-    return res.end('path: ' + req.url + ' method: ' + req.method);
-  } },
-  { method: 'PATCH', path: buildRoutePath('/tasks/:id/complete'), handler: (req, res) => {
-    return res.end('path: ' + req.url + ' method: ' + req.method);
-  } },
-  { method: 'DELETE', path: buildRoutePath('/tasks/:id'), handler: (req, res) => {
-    return res.end('path: ' + req.url + ' method: ' + req.method);
-  } },
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => getAllTasks(req, res),
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => createNewTask(req, res),
+
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => updateTask(req, res),
+
+  },
+  {
+    method: 'PATCH',
+    path: buildRoutePath('/tasks/:id/complete'),
+    handler: (req, res) => completeTask(req, res),
+
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => deleteTask(req, res),
+
+  },
 ];
---
+++ src/service/tasks.js
@@ -0,0 +1,19 @@
+export function getAllTasks(req, res) {
+  return res.end('path: ' + req.url + ' method: ' + req.method);
+}
+
+export function createNewTask(req, res) {
+  return res.end('path: ' + req.url + ' method: ' + req.method);
+}
+
+export function updateTask(req, res) {
+  return res.end('path: ' + req.url + ' method: ' + req.method);
+}
+
+export function completeTask(req, res) {
+  return res.end('path: ' + req.url + ' method: ' + req.method);
+}
+
+export function deleteTask(req, res) {
+  return res.end('path: ' + req.url + ' method: ' + req.method);
+}
